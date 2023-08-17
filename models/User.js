3010b77835e8b7725e4e7634c12012127e0a47fb
Calcifer <shanna.renee.morris@gmail.com>
2023-08-16 20:55:36 -0700

    switched code to the correct file

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+      username: { 
+        type: String,
+        unique: true,
+        required: true,
+        trimmed: true,
+    },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        // look into mongoose validation
+      },
+      thoughts: {
+        // array of id values referencing the thought model
+      },
+      friends: {
+        // array of id values referencing the user model
+      },
+    },
+    {
+      toJSON: {
+        virtuals: true,
+      },
+      id: false,
+    }
+  );
+
+  userSchema
+  .virtual('friendCount')
+  // Getter
+  .get(function () {
+    return `${this.friends.length}`;
+  });
+
+
+const User = model('user', userSchema);
+
+module.exports = User;
