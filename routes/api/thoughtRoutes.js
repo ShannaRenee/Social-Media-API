aeea49af4dbe2592b5a9587321410d5a62f7292b
Calcifer <shanna.renee.morris@gmail.com>
2023-08-20 00:05:25 -0700

    fixing broken routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -11,10 +11,8 @@ const {
   } = require('../../controllers/thoughtController');
   
 //   /api/thoughts
-  router.route('/').get(getThoughts);
+  router.route('/').get(getThoughts).post(createThought);
   
-//   /api/thoughts/:userId
-  router.route('/').post(createThought);
 
 //   /api/thoughts/:thoughtId
   router.route('/:thoughtId')
